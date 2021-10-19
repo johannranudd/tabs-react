7f3319d661d5cd0b584d1c9d7fcb1fb32858e0d2
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-10-19 14:00:13 +0200

    done

--- src/components/Styled.main.js
+++ src/components/Styled.main.js
@@ -3,18 +3,23 @@ import styled from 'styled-components';
 export const StyledMain = styled.main`
   width: 90%;
   margin: auto;
-  max-width: 1200px;
+  max-width: 1100px;
+  letter-spacing: 0.05rem;
+  font-size: 1rem;
   .container {
+    display: grid;
+    /* button container */
     .btn-container {
       display: flex;
       justify-content: center;
-      margin-bottom: 2rem;
+      flex-wrap: wrap;
+      margin-bottom: 3rem;
       .btn {
         color: var(--clr-2);
         background: transparent;
-        border: none;
+        border: 2px solid transparent;
         border-bottom: 2px solid transparent;
-        font-size: 1.4rem;
+        font-size: 1.3rem;
         margin: 0 0.6rem;
         transition: 0.3s linear;
         cursor: pointer;
@@ -29,16 +34,112 @@ export const StyledMain = styled.main`
         border-bottom: 2px solid var(--clr-1);
       }
     }
-    display: grid;
-    .duty {
-      display: grid;
-      grid-template-columns: 10% 90%;
-      place-items: center;
+    /* info container */
+    .info-container {
+      width: 90%;
+      margin: auto;
+      .info-title {
+        font-size: 1.3rem;
+        font-weight: 400;
+        color: var(--clr-2);
+        margin-bottom: 1rem;
+      }
+      .company-subtitle {
+        font-size: 0.9rem;
+        font-weight: 700;
+        color: var(--clr-3);
+        background: var(--clr-4);
+        letter-spacing: 0.08rem;
+        padding: 0.3rem 0.5rem;
+        border-radius: 5px;
+        text-align: center;
+      }
+      .dates {
+        margin: 1rem 0;
+        font-size: 0.9rem;
+        color: var(--clr-3);
+      }
+      .duties {
+        .duty {
+          display: grid;
+          grid-template-columns: 10% 90%;
+          align-items: center;
+          .duty-icon {
+            color: var(--clr-1);
+          }
+          p {
+            margin: 0.5rem 0;
+            font-size: 0.87rem;
+            letter-spacing: 0.01rem;
+            color: var(--clr-6);
+          }
+        }
+      }
     }
   }
+  .more-info-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    .more-info-btn {
+      padding: 0.45rem 3rem;
+      color: var(--clr-7);
+      font-size: 0.85rem;
+      font-weight: 700;
+      letter-spacing: 0.1rem;
+      background: var(--clr-1);
+      border: none;
+      border-radius: 5px;
+      text-transform: uppercase;
+      transition: 0.3s linear;
+      &:hover {
+        color: var(--clr-8);
+        background: var(--clr-9);
+      }
+    }
+  }
+  /* @media (min-width: 790px) {
+    
+  } */
   @media (min-width: 975px) {
     .container {
+      margin-top: 4rem;
       grid-template-columns: 20% 80%;
+      .btn-container {
+        flex-direction: column;
+        justify-content: start;
+        .btn {
+          border: 2px solid transparent;
+          margin-bottom: 1rem;
+          padding: 0;
+          width: 200px;
+          &:hover {
+            border: 2px solid transparent;
+            border-left: 2px solid var(--clr-1);
+          }
+        }
+        .active-btn {
+          border-left: 2px solid var(--clr-1);
+        }
+      }
+      .info-container {
+        .info-title {
+          font-size: 1.4rem;
+        }
+        .company-subtitle {
+        }
+        .dates {
+          font-size: 1rem;
+        }
+        .duties {
+          .duty {
+            p {
+              font-size: 1rem;
+            }
+          }
+        }
+      }
     }
   }
 `;
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -5,10 +5,15 @@ const Tabs = ({ list }) => {
   const [jobs, setJobs] = useState(list);
   const [value, setValue] = useState(0);
   return (
-    <section className='container'>
-      <Buttons jobs={jobs} value={value} setValue={setValue} />
-      <Info jobs={jobs} value={value} />
-    </section>
+    <>
+      <section className='container'>
+        <Buttons jobs={jobs} value={value} setValue={setValue} />
+        <Info jobs={jobs} value={value} />
+      </section>
+      <div className='more-info-container'>
+        <button className='more-info-btn'>More Info</button>
+      </div>
+    </>
   );
 };
 
@@ -33,22 +38,23 @@ const Buttons = ({ jobs, setValue, value }) => {
 const Info = ({ jobs, value }) => {
   const { company, dates, duties, title } = jobs[value];
   return (
-    <article className='info-container'>
-      <h2>{title}</h2>
-      <h3>{company}</h3>
-      <p>{dates}</p>
-      <div className='duties'>
-        {duties.map((duty, index) => {
-          return (
-            <div key={index} className='duty'>
-              <FaAngleDoubleRight />
-              <p>{duty}</p>
-            </div>
-          );
-        })}
-      </div>
-      <button className='more-info-btn'>More Info</button>
-    </article>
+    <>
+      <article className='info-container'>
+        <h2 className='info-title'>{title}</h2>
+        <span className='company-subtitle'>{company}</span>
+        <p className='dates'>{dates}</p>
+        <div className='duties'>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='duty'>
+                <FaAngleDoubleRight className='duty-icon' />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </div>
+      </article>
+    </>
   );
 };
 
